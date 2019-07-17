854543110bd2f3a9d38fb7550edff6d130c73275
Th3Shadowbroker <webmaster@m4taiori.de>
2019-07-17 08:00:21 +0200

    Implemented custom-flag registraion

--- src/org/inventivetalent/bookshelves/Bookshelves.java
+++ src/org/inventivetalent/bookshelves/Bookshelves.java
@@ -31,9 +31,18 @@ public class Bookshelves extends JavaPlugin {
 	String      INVENTORY_TITLE = "Bookshelf";
 	Set<String> disabledWorlds  = new HashSet<>();
 	boolean     onlyBooks       = true;
+	boolean 	worldGuardSupport = false;
 
 	Set<Location> shelves   = new HashSet<>();
 	File          shelfFile = new File(getDataFolder(), "shelves.json");
+
+	@Override
+	public void onLoad() {
+		worldGuardSupport = Bukkit.getPluginManager().getPlugin("WorldGuard") != null;
+		if (worldGuardSupport) {
+			WorldGuardUtils.registerBookshelfAccessFlag();
+		}
+	}
 
 	@Override
 	public void onEnable() {
@@ -83,6 +92,10 @@ public class Bookshelves extends JavaPlugin {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+
+		if (worldGuardSupport) {
+			getLogger().info("Found WorldGuard plugin");
 		}
 
 		Bukkit.getScheduler().runTaskLater(this, new Runnable() {
